812e452400d78091c00681ded99fadea5c626727
Matt <mwbertolucci@gmail.com>
2023-03-29 14:50:13 -0400

    add test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = renderer.create(<BrowserRouter><App /></BrowserRouter>);
+  expect(text).toMatchSnapshot();
 });
